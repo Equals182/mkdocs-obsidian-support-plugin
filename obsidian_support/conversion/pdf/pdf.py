f6d4d1885b7688c03340b3e6226a6a23e5b8193b
Aleksandr <Equals182@users.noreply.github.com>
2025-06-10 17:56:33 +0300

    Trying without base_path

--- obsidian_support/conversion/pdf/pdf.py
+++ obsidian_support/conversion/pdf/pdf.py
@@ -23,14 +23,14 @@ class PdfConversion(AbstractConversion):
     @override
     def convert(self, syntax_groups: SyntaxGroup, page: Page, depth: int) -> str:
         base_path = ""
-        try:
-            if page.canonical_url is None:
-                base_path = page.url
-            else:
-                base_path = page.canonical_url[:-len(page.url)]
-        except TypeError:
-            # joelvaneenwyk todo - hack to skip invalid pdfs for now
-            base_path = ""
+        # try:
+        #     if page.canonical_url is None:
+        #         base_path = page.url
+        #     else:
+        #         base_path = page.canonical_url[:-len(page.url)]
+        # except TypeError:
+        #     # joelvaneenwyk todo - hack to skip invalid pdfs for now
+        #     base_path = ""
         return self._convert_tags(base_path, *syntax_groups)
 
     def _convert_tags(self, base_path: str, pdf_path: str, tags: str) -> str:
